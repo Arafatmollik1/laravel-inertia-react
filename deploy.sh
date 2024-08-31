2a3a0cd7652d4cef10e7c6b63b2100de318d1fb2
Mohammod Arafat Mollik <arafathossain923@gmail.com>
2024-08-31 18:50:45 +0300

    Add github actions for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,31 @@
+#!/bin/bash
 set -e
-echo "Deploying application..."
 
-# Enter maintenance mode
-(php artisan down --message="Upgrading application. We will be back soon.") || true
-# Update codebase
-git pull origin master
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull origin production
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Compile npm assets
+npm run prod
+
+# Run database migrations
+php artisan migrate --force
+
 # Exit maintenance mode
 php artisan up
 
-echo "Application deployed!"
+echo "Deployment finished!"
